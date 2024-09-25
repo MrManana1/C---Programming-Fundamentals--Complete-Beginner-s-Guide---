2dce5ab8cb25f619607eec69d06bc29b1bb18155
Abdul Manan <manan100111@gmail.com>
2024-09-25 14:34:12 +0500

    C++ User Input

---
+++ Cin/cin.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    string namee;
+    cout<<"Please enter your Name: ";
+    cin>>namee;
+    cout<<"Your name is: "<<namee<<endl;
+
+
+    int age;
+    cout<<"Pleae Enter your age: ";
+    cin>>age;
+    cout<<"Your age is:"<<age;
+
+    return 0;
+}
